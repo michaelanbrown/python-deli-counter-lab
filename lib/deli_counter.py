5f1d0e4d64b5d810ec8ddc0851100d1295c68065
michaelanbrown <m_brownbrown@yahoo.com>
2024-08-29 13:01:34 -0400

    determined te place in line

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -3,3 +3,7 @@ katz_deli = []
 def line(katz_deli):
     if not katz_deli:
         print("The line is currently empty.")
+    else:
+        message = "The line is currently:"
+        for i in range(len(katz_deli)):
+            message += f' {i + 1}. {katz_deli[i]}'
